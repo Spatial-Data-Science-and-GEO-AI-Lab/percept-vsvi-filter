1851063965a40875ff545afe98b3deaeb84c5a27
Matthew Danish <m.r.danish@uu.nl>
2024-12-21 23:31:35 +0100

    Add image_width/height to database output, if --seqs given.

--- make_tiles_db.py
+++ make_tiles_db.py
@@ -6,15 +6,20 @@ import os
 from pathlib import Path
 import pickle
 import lzma
+import imagesize
 
 parser = argparse.ArgumentParser(prog='make_tiles_db.py', description='Create single compressed database file with information from tiles JSON files.')
 parser.add_argument('dir', metavar='DIR', help='Directory of tiles JSON files')
 parser.add_argument('--output', '-o', metavar='FILENAME', required=True, help='Write tile database into given FILENAME')
+parser.add_argument('--seqs', type=str, help='Sequences dir (where we can find <seqid>/<imgid>.jpg image files for analysis)', default=None)
 
 def main():
     args = parser.parse_args()
     tiles = Path(args.dir)
     db = {}
+    seqs = Path(args.seqs) if args.seqs else None
+    seqs = seqs if seqs and seqs.exists() else None
+
     def processTilesJson(tilesJson):
         for feat in tilesJson['features']:
             imgid = feat['properties']['id']
@@ -28,6 +33,12 @@ def main():
                 'lon': coord[0],
                 'is_pano': feat['properties']['is_pano']
             }
+            if seqs:
+                imagefile = seqs / Path(seqid) / Path(str(imgid)).with_suffix('.jpg')
+                if imagefile.exists():
+                    w, h = imagesize.get(imagefile)
+                    db[imgid]['image_width'] = w
+                    db[imgid]['image_height'] = h
             #vlog(json.dumps(db[imgid]))
 
     for tilefile in tiles.glob('mly1_public*'):
